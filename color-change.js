8727655528bad047334878e27233d82a3a52066e
sarnegard <sarah.arnegard@gmail.com>
2014-04-09 18:52:15 -0700

    revised variable dec
    changed variable declarations to follow best practices

--- color-change.js
+++ color-change.js
@@ -3,15 +3,12 @@ $(document).ready(function(){
  function isHexColor(char){
         var hexcolor = true; 
         for (var i = 0; i<char.length; i++){
-        // if(isNaN(parseInt(char[i]))){
         
-        //     console.log("not a number");
             if(char[i].match(/[a-fA-F0-9]/)){
                 console.log("a-f");
 
             } else {
                 hexcolor = false; 
-
             }
         }
         console.log(hexcolor);
@@ -23,15 +20,12 @@ document.getElementById("text-1").onkeyup = function getValue() {
 
         console.log("getValue was called");
         var $input = $("#text-1").val();
-        console.log($input);
-        console.log($input.length);
-        // var $count = ($input.length + 1);
-        // console.log($count);
-        
+        var char;
+        var isColor;
+    
         if($input.length == 6 ){
-            // alert("6 has been reached");
-            var char = $input.split("");
-            var isColor = isHexColor(char);
+            char = $input.split("");
+            isColor = isHexColor(char);
             console.log(isColor);
             if((isColor == false) && !($("#error").length)){
                 $('.container').append(
@@ -39,24 +33,12 @@ document.getElementById("text-1").onkeyup = function getValue() {
                     .attr('id', 'error')
                     .text("please enter a valid hex color")
                     );
-                // alert("not a valid hex color");
             } else if (isColor == true){
                 $('body').css({"background-color" : "#" + $input});
             console.log($input);
             }
-
-          
-            
         } else {
             $("#error").remove();
         }
     } 
-
-
-
-
-
-   
-
-
 })
